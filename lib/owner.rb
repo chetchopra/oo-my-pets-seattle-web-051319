4b44ca73b35f286be923bd235aa121200503d834
Chet  Chopra <chopra.chet@gmail.com>
2019-05-16 23:48:06 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -48,6 +48,18 @@ class Owner
       dog.mood = "happy"
     end
   end
+  
+  def play_with_cats
+    @pets[:cats].each do |cat|
+      cat.mood = "happy"
+    end
+  end
+  
+  def feed_fish
+    @pets[:fishes].each do |fish|
+      fish.mood = "happy"
+    end
+  end
     
 
     
