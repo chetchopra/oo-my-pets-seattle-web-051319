431f86e9db429d00834fc9b0359a66cd91c74634
Chet  Chopra <chopra.chet@gmail.com>
2019-05-16 23:33:24 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,10 +1,11 @@
 class Owner
   
-  attr_reader :name
+  attr_reader :name, :species
   @@all = []
   
   def initialize(name)
     @name = name
+    @species = "human"
     @@all << self
   end
   
